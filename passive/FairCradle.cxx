b67b596cecbc7469583710d045efa536860081c8
dbaranov <dmitriybaranof@gmail.com>
2021-08-10 14:56:43 +0300

    CRADLE for the MPD setup has been added as a passive module (ROOT geometry + source files)

---
+++ passive/FairCradle.cxx
@@ -0,0 +1,28 @@
+#include "FairCradle.h"
+
+FairCradle::FairCradle()
+  : FairModule("FairCradle", "") {
+}
+
+FairCradle::FairCradle(const char* name,  const char* title)
+  : FairModule(name ,title) {
+}
+
+FairCradle::~FairCradle() {
+}
+
+void FairCradle::ConstructGeometry() {
+    TString fileName=GetGeometryFileName();
+    if (fileName.EndsWith(".geo")) {
+        ConstructASCIIGeometry();
+    } else if(fileName.EndsWith(".root")) {
+        ConstructRootGeometry();
+    } else {
+        std::cout<< "Geometry format not supported " <<std::endl;
+    }
+}
+
+ClassImp(FairCradle)
+
+
+
