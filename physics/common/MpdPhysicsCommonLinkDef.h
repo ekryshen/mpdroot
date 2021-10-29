76e17ebb944900df687cc6a127955d3746e586db
Daniel Wielanek <daniel.wielanek@gmail.com>
2021-10-29 19:32:38 +0200

    Refactorint of physics directory.

---
+++ physics/common/MpdPhysicsCommonLinkDef.h
@@ -0,0 +1,19 @@
+/*
+ * MpdPhysicsCommonLinkDef.h
+ *
+ *  Created on: 29 paź 2021
+ *      Author: Daniel Wielanek
+ *		E-mail: daniel.wielanek@gmail.com
+ *		Warsaw University of Technology, Faculty of Physics
+ */
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class MpdMiniDstSource + ;
+
+
+#endif
