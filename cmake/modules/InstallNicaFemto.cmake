829c4b9ef0f6a509ee2eb9f30f9c0793c68174a7
daniel.wielanek@gmail.com <wielanek@if.pw.edu.pl>
2021-11-16 17:06:33 +0300

    Newe nicafemto version.

--- cmake/modules/InstallNicaFemto.cmake
+++ cmake/modules/InstallNicaFemto.cmake
@@ -1,6 +1,6 @@
 MACRO(InstallNicaFemto)
 set(NICAFEMTO_SUBDIR_BUILD ON)
-set(NICAFEMTO_HASH c8b3537e0121b0abecf815f89438c946d681751c) 
+set(NICAFEMTO_HASH 7df5fd0a23c047cf46d3523adcf3c17ce97c7125) 
 if(NOT EXISTS "${CMAKE_SOURCE_DIR}/external/nicafemto/CMakeLists.txt")
     execute_process(COMMAND git clone -b master https://git.jinr.ru/nica/nicafemto.git ${CMAKE_SOURCE_DIR}/external/nicafemto)
     execute_process(COMMAND git reset --hard  ${NICAFEMTO_HASH} WORKING_DIRECTORY  ${CMAKE_SOURCE_DIR}/external/nicafemto)
