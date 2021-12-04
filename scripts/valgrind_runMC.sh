fbacfee48d720845740671ab81aef80b84ad996e
Slavomir Hnatic <hnatics@jinr.ru>
2021-12-04 23:02:05 +0300

    remove obsolete files, which need to be updated

--- scripts/valgrind_runMC.sh
+++
@@ -1,9 +0,0 @@
-#bin/bash
-. ~/mpdroot/build/config.sh
-valgrind \
---tool=memcheck \
---leak-check=full \
---log-file=val_runMC.log \
---suppressions=/opt/fairsoft/tools/root/etc/valgrind-root.supp \
-root.exe -l -b -q $VMCWORKDIR/macro/mpd/runMC.C
-
