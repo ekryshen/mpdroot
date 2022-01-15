680f0e3effab6c9caa706b6b124c3b9c1d3d4b24
Slavomir Hnatic <hnatics@jinr.ru>
2022-01-15 11:09:55 +0300

    move bbc detector

--- detectors/bbc/MpdBbcPoint.h
+++ detectors/bbc/MpdBbcPoint.h
@@ -6,7 +6,6 @@
 #ifndef MPDBBCPOINT_H
 #define MPDBBCPOINT_H
 
-
 #include "TObject.h"
 #include "TVector3.h"
 #include "FairMCPoint.h"
@@ -14,37 +13,29 @@
 using namespace std;
 
 //------------------------------------------------------------------------------------------------------------------------
-class MpdBbcPoint : public FairMCPoint
-{
+class MpdBbcPoint : public FairMCPoint {
 
 public:
+   /** Constructor with arguments
+    *@param trackID  Index of MCTrack
+    *@param detID    Detector ID
+    *@param pos      Ccoordinates at entrance to active volume [cm]
+    *@param mom      Momentum of track at entrance [GeV]
+    *@param tof      Time since event start [ns]
+    *@param length   Track length since creation [cm]
+    *@param eLoss    Energy deposit [GeV]
+    **/
+   MpdBbcPoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, Double_t tof, Double_t length, Double_t eLoss);
+   MpdBbcPoint(const MpdBbcPoint &point) { *this = point; };
 
-  /** Constructor with arguments
-   *@param trackID  Index of MCTrack
-   *@param detID    Detector ID
-   *@param pos      Ccoordinates at entrance to active volume [cm]
-   *@param mom      Momentum of track at entrance [GeV]
-   *@param tof      Time since event start [ns]
-   *@param length   Track length since creation [cm]
-   *@param eLoss    Energy deposit [GeV]
-   **/
-  	MpdBbcPoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, Double_t tof, Double_t length, Double_t eLoss);
-  	MpdBbcPoint(const MpdBbcPoint& point) { *this = point; };
+   MpdBbcPoint();
+   virtual ~MpdBbcPoint();
 
-  	MpdBbcPoint();	
-  	virtual ~MpdBbcPoint();
+   // Output to screen
+   virtual void Print(const Option_t *opt) const;
 
-	// Output to screen 
-  	virtual void Print(const Option_t* opt) const;
-
-  ClassDef(MpdBbcPoint,1)
-
+   ClassDef(MpdBbcPoint, 1)
 };
 
 //------------------------------------------------------------------------------------------------------------------------
 #endif
-
-
-
-
-
