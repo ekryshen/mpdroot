9382404bd5914c6fde39b4df0bff2b9069bc48eb
Jan Busa <busa@jinr.ru>
2022-06-06 12:40:49 +0300

    Resolve "Invalid MpdTpcDigit branch in root files"

--- detectors/tpc/MpdTpcDigit.h
+++ detectors/tpc/MpdTpcDigit.h
@@ -20,9 +20,7 @@ class MpdTpcDigit : public TObject {
 
 public:
    MpdTpcDigit();
-   // AZ MpdTpcDigit(MpdTpcDigit* digit);
    MpdTpcDigit(Int_t ori, Int_t pad, Int_t row, Int_t bin, Int_t sec, Float_t adc);
-   MpdTpcDigit(Int_t ori, Int_t pad, Int_t row, Int_t bin, Int_t sec, Float_t adc, std::map<Int_t, Float_t> &origins);
    virtual ~MpdTpcDigit();
 
    Int_t   GetOrigin() const { return fOrigin; }
@@ -31,9 +29,7 @@ public:
    Int_t   GetTimeBin() const { return fTimeBin; }
    Float_t GetAdc() const { return fAdc; }
    Int_t   GetSector() const { return fSector; }
-   Int_t   GetNorigins() const { return fOrigins.size(); }
 
-   // AZ void SetOrigin (Int_t ori)  {fOrigin = ori;}
    void SetPad(Int_t pad) { fPad = pad; }
    void SetRow(Int_t row) { fRow = row; }
    void SetTimeBin(Int_t bin) { fTimeBin = bin; }
@@ -41,17 +37,14 @@ public:
    void SetSector(Int_t sec) { fSector = sec; }
 
 private:
-   Int_t fPad;
-   Int_t fRow;
-   Int_t fTimeBin;
-   Int_t fSector;
-   // AZ Float_t fAdc;
-   Double32_t               fAdc;
-   Int_t                    fOrigin;
-   Double32_t               fOrigWeight;
-   std::map<Int_t, Float_t> fOrigins;
+   Int_t   fPad;
+   Int_t   fRow;
+   Int_t   fTimeBin;
+   Int_t   fSector;
+   Float_t fAdc;
+   Int_t   fOrigin;
 
-   ClassDef(MpdTpcDigit, 2);
+   ClassDef(MpdTpcDigit, 3);
 };
 
 #endif
--- macros/common/runReco.C
+++ macros/common/runReco.C
@@ -36,7 +36,7 @@
 
 #include <iostream>
 
-#define UseMlem  // Choose: UseMlem HitProducer
+#define UseMlem // Choose: UseMlem HitProducer
 
 #include "commonFunctions.C"
 
@@ -48,171 +48,170 @@
 // run_type - proof execution, default "local". e.g.:
 //      "proof" - run on proof-lite with "CPU" count workers,
 //      "proof:workers=3" - run on proof-lite with 3 workers
-//      "proof:user@proof.server:21001" - to run on the PROOF cluster created with PoD (under user 'MPD', default port - 21001)
-//      "proof:user@proof.server:21001:workers=10" - to run on the PROOF cluster created with PoD with 10 workers (under USER, default port - 21001)
+//      "proof:user@proof.server:21001" - to run on the PROOF cluster created with PoD (under user 'MPD', default port -
+//      21001) "proof:user@proof.server:21001:workers=10" - to run on the PROOF cluster created with PoD with 10 workers
+//      (under USER, default port - 21001)
 //	nc-farm : proof:mpd@nc10.jinr.ru:21001
-void runReco(TString inFile = "evetest.root", TString outFile = "mpddst.root", Int_t nStartEvent = 0, Int_t nEvents = 10, TString run_type = "local") {
+void runReco(TString inFile = "evetest.root", TString outFile = "mpddst.root", Int_t nStartEvent = 0,
+             Int_t nEvents = 10, TString run_type = "local")
+{
 
-    // ========================================================================
-    // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
-    Int_t iVerbose = 0;
+   // ========================================================================
+   // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug)
+   Int_t iVerbose = 0;
 
-    // -----   Timer   --------------------------------------------------------
-    TStopwatch timer;
-    timer.Start();
+   // -----   Timer   --------------------------------------------------------
+   TStopwatch timer;
+   timer.Start();
 
-    // -----   Digitization run   -------------------------------------------
-    // define parallel or sequential execution
-    int ind = run_type.Index(':');
-    TString proof_name = "";
-    if (ind >= 0) {
-        proof_name = run_type(ind + 1, run_type.Length() - ind - 1);
-        run_type = run_type(0, ind);
-    }
+   // -----   Digitization run   -------------------------------------------
+   // define parallel or sequential execution
+   int     ind        = run_type.Index(':');
+   TString proof_name = "";
+   if (ind >= 0) {
+      proof_name = run_type(ind + 1, run_type.Length() - ind - 1);
+      run_type   = run_type(0, ind);
+   }
 
-    FairRunAna* fRun;
-    if (run_type == "proof")
-    {
-        fRun = new FairRunAnaProof(proof_name);
-        ((FairRunAnaProof*)fRun)->SetProofParName("$CONFIG_DIR/libMpdRoot.par");
-    }
-    else
-    {
-        if (!CheckFileExist(inFile)) return;
-        fRun = new FairRunAna();
-    }
+   FairRunAna *fRun;
+   if (run_type == "proof") {
+      fRun = new FairRunAnaProof(proof_name);
+      ((FairRunAnaProof *)fRun)->SetProofParName("$CONFIG_DIR/libMpdRoot.par");
+   } else {
+      if (!CheckFileExist(inFile)) return;
+      fRun = new FairRunAna();
+   }
 
-    FairSource* fFileSource = new FairFileSource(inFile);
-    fRun->SetSource(fFileSource);
-    fRun->SetOutputFile(outFile);
-    fRun->SetGenerateRunInfo(false);
-    fRun->SetUseFairLinks(true);
-    // ------------------------------------------------------------------------
+   FairSource *fFileSource = new FairFileSource(inFile);
+   fRun->SetSource(fFileSource);
+   fRun->SetOutputFile(outFile);
+   fRun->SetGenerateRunInfo(false);
+   fRun->SetUseFairLinks(true);
+   // ------------------------------------------------------------------------
 
-    // Parameter file
-    TString parFile = inFile;
+   // Parameter file
+   TString parFile = inFile;
 
-    // -----  Parameter database   --------------------------------------------
-    FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
-    FairParRootFileIo* parIo1 = new FairParRootFileIo();
-    parIo1->open(parFile.Data());
-    rtdb->setFirstInput(parIo1);
-    rtdb->setOutput(parIo1);
-    rtdb->saveOutput();
-    // ------------------------------------------------------------------------
+   // -----  Parameter database   --------------------------------------------
+   FairRuntimeDb     *rtdb   = fRun->GetRuntimeDb();
+   FairParRootFileIo *parIo1 = new FairParRootFileIo();
+   parIo1->open(parFile.Data());
+   rtdb->setFirstInput(parIo1);
+   rtdb->setOutput(parIo1);
+   rtdb->saveOutput();
+   // ------------------------------------------------------------------------
 
-    MpdKalmanFilter *kalman = MpdKalmanFilter::Instance("KF");
-    fRun->AddTask(kalman);
+   MpdKalmanFilter *kalman = MpdKalmanFilter::Instance("KF");
+   fRun->AddTask(kalman);
 
 #ifdef UseMlem
-    //MpdTpcDigitizerAZ* tpcDigitizer = new MpdTpcDigitizerAZ();
-    MpdTpcDigitizerAZlt* tpcDigitizer = new MpdTpcDigitizerAZlt();
-    tpcDigitizer->SetPersistence(kFALSE);
-    fRun->AddTask(tpcDigitizer);
+   // MpdTpcDigitizerAZ* tpcDigitizer = new MpdTpcDigitizerAZ();
+   MpdTpcDigitizerAZlt *tpcDigitizer = new MpdTpcDigitizerAZlt();
+   tpcDigitizer->SetPersistence(kTRUE);
+   fRun->AddTask(tpcDigitizer);
 #endif
 
-    //  MpdTpcClusterFinderTask *tpcClusterFinder = new MpdTpcClusterFinderTask();
-    //  tpcClusterFinder->SetDebug(kFALSE);
-    //  tpcClusterFinder->SetMakeQA(kTRUE);
-    //  tpcClusterFinder->SetCalcResiduals(kFALSE);
-    //  fRun->AddTask(tpcClusterFinder);
+   //  MpdTpcClusterFinderTask *tpcClusterFinder = new MpdTpcClusterFinderTask();
+   //  tpcClusterFinder->SetDebug(kFALSE);
+   //  tpcClusterFinder->SetMakeQA(kTRUE);
+   //  tpcClusterFinder->SetCalcResiduals(kFALSE);
+   //  fRun->AddTask(tpcClusterFinder);
 
 #ifdef UseMlem
-    MpdTpcClusterFinderMlem *tpcClusAZ = new MpdTpcClusterFinderMlem();
-    fRun->AddTask(tpcClusAZ);
+   MpdTpcClusterFinderMlem *tpcClusAZ = new MpdTpcClusterFinderMlem();
+   fRun->AddTask(tpcClusAZ);
 #else
-    MpdTpcHitProducer* hitPr = new MpdTpcHitProducer();
-    hitPr->SetModular(0);
-    fRun->AddTask(hitPr);
+   MpdTpcHitProducer *hitPr = new MpdTpcHitProducer();
+   hitPr->SetModular(0);
+   fRun->AddTask(hitPr);
 #endif
 
-    FairTask* vertZ = new MpdVertexZfinder();
-    fRun->AddTask(vertZ);
+   FairTask *vertZ = new MpdVertexZfinder();
+   fRun->AddTask(vertZ);
 
-    MpdTpcKalmanFilter* recoKF = new MpdTpcKalmanFilter("Kalman filter");
+   MpdTpcKalmanFilter *recoKF = new MpdTpcKalmanFilter("Kalman filter");
 #ifdef UseMlem
-    recoKF->UseTpcHit(kFALSE); // do not use hits from the hit producer
+   recoKF->UseTpcHit(kFALSE); // do not use hits from the hit producer
 #endif
-    fRun->AddTask(recoKF);
+   fRun->AddTask(recoKF);
 
-    FairTask* findVtx = new MpdKfPrimaryVertexFinder("Vertex finder");
-    fRun->AddTask(findVtx);
+   FairTask *findVtx = new MpdKfPrimaryVertexFinder("Vertex finder");
+   fRun->AddTask(findVtx);
 
-    MpdFfdHitProducer* ffdHit = new MpdFfdHitProducer("FFDHitProducer");
-    fRun->AddTask(ffdHit);
+   MpdFfdHitProducer *ffdHit = new MpdFfdHitProducer("FFDHitProducer");
+   fRun->AddTask(ffdHit);
 
-    // TOF hit producers
-    MpdTofHitProducer* tofHit = new MpdTofHitProducer("Hit producer");
-    tofHit->SetTimeResolution(0.080);
-    fRun->AddTask(tofHit);
+   // TOF hit producers
+   MpdTofHitProducer *tofHit = new MpdTofHitProducer("Hit producer");
+   tofHit->SetTimeResolution(0.080);
+   fRun->AddTask(tofHit);
 
-/*
-    MpdEtofHitProducer* etofHitProd = new MpdEtofHitProducer("ETOF HitProducer");
-    fRun->AddTask(etofHitProd);
-    
-    // Endcap tracking
-    FairTask* tpcECT = new MpdEctTrackFinderTpc(); // undefined class
-    tpcECT->SetVerbose(iVerbose);
-    fRun->AddTask(tpcECT);
-    
-    MpdEctTrackFinderCpc* tofECT = new MpdEctTrackFinderCpc(); // undefined class
-    tofECT->SetVerbose(iVerbose);
-    tofECT->SetTpc(kTRUE);
-    fRun->AddTask(tofECT);
-*/
+   /*
+       MpdEtofHitProducer* etofHitProd = new MpdEtofHitProducer("ETOF HitProducer");
+       fRun->AddTask(etofHitProd);
 
-    // TOF matching
-    MpdTofMatching* tofMatch = new MpdTofMatching("TOF matching");
-    fRun->AddTask(tofMatch);
+       // Endcap tracking
+       FairTask* tpcECT = new MpdEctTrackFinderTpc(); // undefined class
+       tpcECT->SetVerbose(iVerbose);
+       fRun->AddTask(tpcECT);
 
-    // ETOF matching
-    //MpdEtofMatching* etofMatch = new MpdEtofMatching("ETOF matching");
-    //fRun->AddTask(etofMatch);
+       MpdEctTrackFinderCpc* tofECT = new MpdEctTrackFinderCpc(); // undefined class
+       tofECT->SetVerbose(iVerbose);
+       tofECT->SetTpc(kTRUE);
+       fRun->AddTask(tofECT);
+   */
 
-    //FairTask *emcHP = new MpdEmcHitCreation();
-    //fRun->AddTask(emcHP);
+   // TOF matching
+   MpdTofMatching *tofMatch = new MpdTofMatching("TOF matching");
+   fRun->AddTask(tofMatch);
 
-    FairTask *tdigi = new MpdZdcDigiProducer("MpdZdcDigiProducer");
-    fRun->AddTask(tdigi);
+   // ETOF matching
+   // MpdEtofMatching* etofMatch = new MpdEtofMatching("ETOF matching");
+   // fRun->AddTask(etofMatch);
 
-    //MpdPidRefitTrackTask* trRefit = new MpdPidRefitTrackTask("Track PID and Refit");
-    //fRun->AddTask(trRefit);
+   // FairTask *emcHP = new MpdEmcHitCreation();
+   // fRun->AddTask(emcHP);
 
-    MpdFillDstTask* fillDST = new MpdFillDstTask("MpdDst task");
-    fRun->AddTask(fillDST);
-    
-    MpdMiniDstFillTask* miniDst = new MpdMiniDstFillTask(outFile);
-    fRun->AddTask(miniDst);
-    
-    // -----   Intialise   ----------------------------------------------------
-    fRun->Init();
-    if (run_type != "proof") cout<<"Field: "<<fRun->GetField()->GetBz(0., 0., 0.)<<endl;
-    else {
-        TProof* pProof = ((FairRunAnaProof*)fRun)->GetProof();
-        pProof->SetParameter("PROOF_PacketizerStrategy", (Int_t) 0);
-        ind = proof_name.Index(":workers=");
-        if (ind >= 0) {
-            TString worker_count = proof_name(ind + 9, proof_name.Length() - ind - 9);
-            if (worker_count.IsDigit())
-                pProof->SetParallel(worker_count.Atoi());
-        }
-    }
+   FairTask *tdigi = new MpdZdcDigiProducer("MpdZdcDigiProducer");
+   fRun->AddTask(tdigi);
 
-    // if nEvents is equal 0 then all events of the given file starting with "nStartEvent" should be processed
-    if (nEvents == 0)
-        nEvents = MpdGetNumEvents::GetNumROOTEvents((char*)inFile.Data()) - nStartEvent;
+   // MpdPidRefitTrackTask* trRefit = new MpdPidRefitTrackTask("Track PID and Refit");
+   // fRun->AddTask(trRefit);
 
-    // -----   Run   ______________--------------------------------------------
-    fRun->Run(nStartEvent, nStartEvent + nEvents);
+   MpdFillDstTask *fillDST = new MpdFillDstTask("MpdDst task");
+   fRun->AddTask(fillDST);
 
-    // -----   Finish   -------------------------------------------------------
-    timer.Stop();
-    Double_t rtime = timer.RealTime();
-    Double_t ctime = timer.CpuTime();
-    cout << endl << endl;
-    cout << "Macro finished successfully." << endl;      // marker of successful execution for CDASH
-    cout << "Output file is " << outFile << endl;
-    cout << "Parameter file is " << parFile << endl;
-    cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
-    cout << endl;
+   MpdMiniDstFillTask *miniDst = new MpdMiniDstFillTask(outFile);
+   fRun->AddTask(miniDst);
+
+   // -----   Intialise   ----------------------------------------------------
+   fRun->Init();
+   if (run_type != "proof")
+      cout << "Field: " << fRun->GetField()->GetBz(0., 0., 0.) << endl;
+   else {
+      TProof *pProof = ((FairRunAnaProof *)fRun)->GetProof();
+      pProof->SetParameter("PROOF_PacketizerStrategy", (Int_t)0);
+      ind = proof_name.Index(":workers=");
+      if (ind >= 0) {
+         TString worker_count = proof_name(ind + 9, proof_name.Length() - ind - 9);
+         if (worker_count.IsDigit()) pProof->SetParallel(worker_count.Atoi());
+      }
+   }
+
+   // if nEvents is equal 0 then all events of the given file starting with "nStartEvent" should be processed
+   if (nEvents == 0) nEvents = MpdGetNumEvents::GetNumROOTEvents((char *)inFile.Data()) - nStartEvent;
+
+   // -----   Run   ______________--------------------------------------------
+   fRun->Run(nStartEvent, nStartEvent + nEvents);
+
+   // -----   Finish   -------------------------------------------------------
+   timer.Stop();
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   cout << endl << endl;
+   cout << "Macro finished successfully." << endl; // marker of successful execution for CDASH
+   cout << "Output file is " << outFile << endl;
+   cout << "Parameter file is " << parFile << endl;
+   cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+   cout << endl;
 }
