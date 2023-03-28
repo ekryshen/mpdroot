d3f383ce6d405ddeeed6e0f0bd9e7efc364c6dd2
ParfenovPeter <terrylapard@gmail.com>
2023-03-28 23:33:47 +0300

    Added first implementation of the evPlane wagon for the event plane measurements using FHCal and TPC. Recentering and shift EP corrections are implemented as well.

---
+++ physics/MpdAnalysisEventPlane.cxx
@@ -0,0 +1,12 @@
+#include "MpdAnalysisEventPlane.h"
+ClassImp(MpdAnalysisEventPlane);
+
+MpdAnalysisEventPlane::MpdAnalysisEventPlane() : fPhiEP_FHCal_F_all(-9999.),
+    fPhiEP_FHCal_N_all(-9999.),
+    fPhiEP_FHCal_S_all(-9999.),
+    fPhiEP_TPC_N_all(-9999.),
+    fPhiEP_TPC_S_all(-9999.)
+{}
+
+MpdAnalysisEventPlane::~MpdAnalysisEventPlane()
+{}
